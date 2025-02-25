a55a637f74bacf49e3530c7d09573d809d9c4c05
Kristoffer Blucher <kristofferblucher@gmail.com>
2025-02-25 12:13:39 +0100

    new try

--- FootballApi/Program.cs
+++ FootballApi/Program.cs
@@ -2,28 +2,26 @@ using Microsoft.AspNetCore.Builder;
 using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.Extensions.Options;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<PlayerService>();
 
-
+// Configure CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowWebApp",
-        policy => policy.WithOrigins("http://localhost:5173")  // Add the origins you want to allow
-                        .AllowAnyHeader()
-                        .AllowAnyMethod());
+        policy => policy
+            .WithOrigins("http://localhost:5173", "https://football-frontend.onrender.com") // Add your frontend URL here
+            .AllowAnyHeader()
+            .AllowAnyMethod());
 });
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,18 +29,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapControllers();
-
 
 app.UseRouting();
 
 // Apply CORS policy
 app.UseCors("AllowWebApp");
 
-// Add this line to disable HTTPS redirection
-builder.Services.AddHttpsRedirection(options =>
-{
-    options.HttpsPort = null;
-});
+// Ensure HTTP redirection works correctly for production
+app.UseHttpsRedirection();
 
-app.Run("http://0.0.0.0:5105");
+app.MapControllers();
+
+app.Run();
